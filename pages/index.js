83b5f06635b09e5036018f1a3bca801036947fc3
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-20 18:30:19 -0400

    goog api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-export default function Home() {
+import { useSession, signIn, signOut } from "next-auth/react"
 
-  return (
-    <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
+export default function Home() {
+  const { data: session } = useSession()
+  if (!session) {
+    return (
+      <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
       <div className="text-center w-full">
-         <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+         <button onClick={() => signIn()}  className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
       </div>
     </div>
-  )
+    );
+  }
+  return (
+    <div>Logged In {session.user.email}</div>
+  );
 }
