315456aa72ca1d5344b2f35f5c18461f8b955fed
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-20 23:08:52 -0400

    about to start the navbar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Nav from "@/components/nav";
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
   }
   return (
     <div className="bg-blue-900 min-h-screen">
+    
        <div>Logged In {session.user.email}</div>
     </div>
   );
