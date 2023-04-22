8b155ed4be7f04a75096e69f5a25d27a2c7b07e3
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-22 08:34:26 -0400

    Fixed the white one

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,16 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./component/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5542F6',
+        highlight: '#eae8fb',
+        bgGray: '#fbfafd',
+      },
+    },
   },
   plugins: [],
 }
