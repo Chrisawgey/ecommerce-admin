d695a6cb2c8e68518c75c1802de3e31f426dd417
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-22 08:03:01 -0400

    redo the whole thing with navbar

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   }
   return (
     <div className="bg-blue-900 min-h-screen">
-    
+      <Nav />
        <div>Logged In {session.user.email}</div>
     </div>
   );
