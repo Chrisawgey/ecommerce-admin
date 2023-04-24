d832b781be156f3ce01aae17fb02c482069d46ce
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-24 16:09:51 -0400

    adding href links and overall basics

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home() {
     );
   }
   return (
-    <div className="bg-blue-900 min-h-screen">
+    <div className="bg-blue-900 min-h-screen flex">
       <Nav />
-       <div>Logged In {session.user.email}</div>
+       <div className="bg-white flex grow mt-2 mr-2 mb-2 rounded-lg p-4">Logged In {session.user.email}</div>
     </div>
   );
 }
