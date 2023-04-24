a96b143161989b869785dbd726fdf9f96e62da4e
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-24 16:37:08 -0400

    href plus creating path names

--- components/nav.js
+++ components/nav.js
@@ -1,8 +1,11 @@
 import Link from "next/link"
+import { useRouter } from "next/router";
 
 export default function Nav () {
     const inactiveLink = 'flex gap-1 p-1';
     const activeLink = inactiveLink+' bg-white text-blue-900 rounded-l-lg';
+    const router = useRouter();
+    const {pathname} = router;
 
     return (
         <aside className="text-white p-4 pr-0">
@@ -15,13 +18,13 @@ export default function Nav () {
             </span>
             </Link>
             <nav className="flex flex-col gap-2">
-                <Link href={'/'} className={activeLink}>
+                <Link href={'/'} className={pathname === '/' ? activeLink : inactiveLink}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
                 Dashboard
                 </Link>
-                <Link href={'/products'} className={inactiveLink}>
+                <Link href={'/products'} className={pathname.includes('/products') ? activeLink : inactiveLink}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
                 </svg>
---
+++ pages/products.js
@@ -0,0 +1,7 @@
+import Layout from "@/components/layout";
+
+export default function Products() {
+    return (
+        <Layout>Products page here leave</Layout>
+    );
+}
