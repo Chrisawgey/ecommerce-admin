c61b4564a7227aacec52527810e617cdaee60ec3
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-25 18:14:12 -0400

    not done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Layout from "@/components/layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const {data: session} = useSession();
+  console.log({session});
   return <Layout>
-    test
+    <div className="text-blue-900">
+      Welcome back, {session?.user?.name}
+      <img src={session?Home,user?.image}
+    </div>
   </Layout>
 }
