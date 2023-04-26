ba2d27827cb3575c886cbc47eed15751c4e7607f
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-25 22:31:37 -0400

    fixing needed

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import Layout from "@/components/layout";
-import { useSession } from "next-auth/react";
+import {useSession} from "next-auth/react";
 
 export default function Home() {
   const {data: session} = useSession();
   return <Layout>
-    <div className="text-blue-900 flex justify-between">
-    <h2>
-    Welcome back, {session?.user?.name}
-    </h2>
-      <div className="flex bg-gray-300 gap-1 text-black">
+    <div className="text-blue-900 flex">
+      <h2>
+        Hello, {session?.user?.name}
+      </h2> 
+      <div className="flex bg-gray-200 text-black">
         <img src={session?.user?.image} alt="" className="w-6 h-6"/>
         {session?.user?.name}
-      </div>
+      </div> 
     </div>
-  </Layout>
-};
+</Layout>
+}
