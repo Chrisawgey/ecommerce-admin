e179f511e72fdaea1dba7752488d2b1b85e63ddb
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-26 16:50:19 -0400

    trying to fix justify-beytween

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,11 @@ export default function Home() {
   return <Layout>
   <div>
     <div className="text-blue-900 flex justify-between">
+    <div>
       <h2>
         Hello, {session?.user?.name}
       </h2> 
+      </div>
       <div className="flex bg-gray-300 gap-1 text-black">
         <img src={session?.user?.image} alt="" className="w-8 h-8"/>
         <span className="py-1 px-2">
