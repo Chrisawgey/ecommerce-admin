49ea11b2890ceca1412694a54d86d3a7ac46b124
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-25 21:48:54 -0400

    picture of user using goog

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,13 @@ export default function Home() {
   const {data: session} = useSession();
   console.log({session});
   return <Layout>
-    <div className="text-blue-900">
-      Welcome back, {session?.user?.name}
-      <img src={session?Home,user?.image}
+    <div className="text-blue-900 flex">
+    <h2>
+    Welcome back, {session?.user?.name}
+    </h2>
+      <div>
+        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+      </div>
     </div>
   </Layout>
 }
