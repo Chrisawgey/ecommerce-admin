7afbda751aa7188c0b0c770385ccd04b62fbc0e7
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-25 22:09:07 -0400

    issue

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,15 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const {data: session} = useSession();
-  console.log({session});
   return <Layout>
-    <div className="text-blue-900 flex">
+    <div className="text-blue-900 flex justify-between">
     <h2>
     Welcome back, {session?.user?.name}
     </h2>
-      <div>
+      <div className="flex bg-gray-300 gap-1 text-black">
         <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+        {session?.user?.name}
       </div>
     </div>
   </Layout>
-}
+};
