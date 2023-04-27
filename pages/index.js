a5e84cc1b1a5bd7b9bf193882bbe0f273f4be43c
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-26 21:51:30 -0400

    stuff

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,16 @@ import {useSession} from "next-auth/react";
 export default function Home() {
   const {data: session} = useSession();
   return <Layout>
-  <div>
-    <div className="text-blue-900 flex justify-between">
     <div>
-      <h2>
-        Hello, {session?.user?.name}
-      </h2> 
-      </div>
-      <div className="flex bg-gray-300 gap-1 text-black">
-        <img src={session?.user?.image} alt="" className="w-8 h-8"/>
-        <span className="py-1 px-2">
-        {session?.user?.name}
+    <div className="text-blue-900 flex justify-between">
+      <h2> Hello, <b>{session?.user?.name}</b></h2>
+       <div className="flex gap-1 text-black rounded-lg overflow-hidden">
+      <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+        <span className="px-2">
+        
         </span>
         </div>
-      </div> 
-    </div>
+         </div>
+        </div>
 </Layout>
 }
--- pages/products.js
+++ pages/products.js
@@ -1,7 +1,10 @@
 import Layout from "@/components/layout";
+import Link from "next/link";
 
 export default function Products() {
     return (
-        <Layout>Products page here leave</Layout>
+        <Layout>
+            <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new product</Link>
+        </Layout>
     );
 }
