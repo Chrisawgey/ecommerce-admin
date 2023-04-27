8c3d37e36bc433da6f91dafe5af1257e4a3d08ea
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-27 16:37:18 -0400

    fixed bg

--- pages/products.js
+++ pages/products.js
@@ -2,9 +2,11 @@ import Layout from "@/components/layout";
 import Link from "next/link";
 
 export default function Products() {
-    return (
-        <Layout>
-            <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new product</Link>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Link className="py-1 px-2" href={"/products/new"}>
+        <span className="bg-blue-900 text-white rounded-md">Add new product</span>
+      </Link>
+    </Layout>
+  );
 }
