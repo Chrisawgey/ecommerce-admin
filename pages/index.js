079fd78eb97380128de62d651e2eb2509ca07c5b
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-27 10:27:33 -0400

    user image and name is fixed

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,18 @@ import {useSession} from "next-auth/react";
 export default function Home() {
   const {data: session} = useSession();
   return <Layout>
-    <div>
+  <div className="w-full">
     <div className="text-blue-900 flex justify-between">
-      <h2> Hello, <b>{session?.user?.name}</b></h2>
-       <div className="flex gap-1 text-black rounded-lg overflow-hidden">
+      <h2>
+        Hello, <b>{session?.user?.name}</b>
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
       <img src={session?.user?.image} alt="" className="w-6 h-6"/>
         <span className="px-2">
-        
-        </span>
+        {session?.user?.name}
+            </span>
+          </div>
         </div>
-         </div>
-        </div>
+      </div> 
 </Layout>
 }
