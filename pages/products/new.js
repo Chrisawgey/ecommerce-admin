69f1b0b6342293ea92edec2dc3ef645213b7b707
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-28 09:07:19 -0400

    bubbles

--- pages/products/new.js
+++ pages/products/new.js
@@ -3,6 +3,7 @@ import Layout from "@/components/layout";
 export default function NewProduct() {
     return (
         <Layout>
+        <h1>New Product</h1>
            <span className="w-full">
             <input type="text" placeholder="product name"/>
             <textarea placeholder="description"></textarea>
