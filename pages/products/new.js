f3a039618299ca402b8d2fca426a269dd7064480
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-28 11:04:01 -0400

    labels and input type plcaeholder

--- pages/products/new.js
+++ pages/products/new.js
@@ -4,9 +4,13 @@ export default function NewProduct() {
     return (
         <Layout>
             <span className="w-full">
-            <h1 className="text-blue-900 mb-2"> New Product</h1>
+            <h1> New Product</h1>
+            <label>Products Name</label>
             <input type="text" placeholder="product name"/>
+            <label>Description</label>
             <textarea placeholder="description"></textarea>
+            <label>Price (in USD) </label>
+            <input type="number" placeholder="price"/>
             </span>
         </Layout>
     )
