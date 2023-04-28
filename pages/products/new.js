398eede9bc30eb438eac724721d98d1cc99748a7
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-28 10:53:03 -0400

    fixed

--- pages/products/new.js
+++ pages/products/new.js
@@ -3,8 +3,8 @@ import Layout from "@/components/layout";
 export default function NewProduct() {
     return (
         <Layout>
-        <h1>New Product</h1>
-           <span className="w-full">
+            <span className="w-full">
+            <h1> New Product</h1>
             <input type="text" placeholder="product name"/>
             <textarea placeholder="description"></textarea>
             </span>
