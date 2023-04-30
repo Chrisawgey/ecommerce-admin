fc02bc208285d69bda12047528b2577c53c0c38c
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-04-30 19:54:54 -0400

    px

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </h2>
       <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
       <img src={session?.user?.image} alt="" className="w-6 h-6"/>
-        <span className="px-2">
+        <span className="px-1">
         {session?.user?.name}
             </span>
           </div>
