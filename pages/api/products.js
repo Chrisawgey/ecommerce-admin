a9df7cd6870f7898a44635959d18fc8efd2cfcef
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-01 20:49:59 -0400

    getting api issues

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 import { Product } from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
 
 export default async function handle(req, res){
     const {method} = req;
-
+    await mongooseConnect();
     if (method === 'POST'){
         const {title,description,price} = req.body;
         const productDoc = await Product.create({
