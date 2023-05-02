f5b8497a22f61a1326b65a87697c7c867c211966
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-01 20:32:45 -0400

    deleting clientPromise

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-import clientPromise from "@/lib/mongods";
+
 import { Product } from "@/models/Product";
 
 export default async function handle(req, res){
     const {method} = req;
-    mongoose.Promise = clientPromise;
+
     if (method === 'POST'){
         const {title,description,price} = req.body;
         const productDoc = await Product.create({
