f602c9185bc58bd486ec9b73e688e5ef17c62463
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-01 20:15:54 -0400

    mongoose url +

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,6 +1,9 @@
+import mongoose from "mongoose";
+
 export default function handle(req, res){
     const {method} = req;
+    mongoose.connect(clientPromise.url);
     if (method === 'POST'){
-        
+        res.json('post');
     }
 }
