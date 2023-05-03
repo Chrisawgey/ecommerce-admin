852347727134f749be46cdc130ad822ffae6cb11
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-03 15:06:59 -0400

    maybe fix

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,7 +1,6 @@
-
+import mongoose from "mongoose";
 import {mongooseConnect} from "@/lib/mongoose";
 import {Product} from "@/models/Product";
-import mongoose from "mongoose";
 
 export default async function handle(req, res) {
     const {method} = req;
@@ -11,6 +10,6 @@ export default async function handle(req, res) {
         const productDoc = await Product.create({
             title,description,price,
         })
-        res.json('productDoc');
+        res.json(productDoc);
     }
 }
