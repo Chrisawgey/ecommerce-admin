300a23b813a09371f032fade748a59463338294e
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-10 12:07:10 -0400

    collab fix(ft raf) fixing parent

--- components/layout.js
+++ components/layout.js
@@ -15,7 +15,7 @@ export default function Layout({children}) {
   return (
     <div className="bg-blue-900 min-h-screen flex">
       <Nav />
-       <div className="bg-white flex grow mt-2 mr-2 mb-2 rounded-lg p-4">
+       <div className="bg-white  grow mt-2 mr-2 mb-2 rounded-lg p-4">
         {children}
        </div>
     </div>
--- pages/products.js
+++ pages/products.js
@@ -14,7 +14,7 @@ export default function Products() {
     <Layout>
       <Link className="bg-blue-900 text-white rounded-md py-1 px-2" 
       href={'/products/new'}>Add new product</Link>
-       <table className="basic mt-2">
+      <table className="basic mt-2">
         <thead>
           <tr>
             <td>Product name</td>
