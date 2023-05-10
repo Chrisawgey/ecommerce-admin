9fd503a0f682d76c734e48490fd60039413f0532
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-10 11:45:31 -0400

    better looking

--- pages/products.js
+++ pages/products.js
@@ -12,7 +12,8 @@ export default function Products() {
   }, []);
   return (
     <Layout>
-      <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new product</Link>
+      <Link className="bg-blue-900 text-white rounded-md py-1 px-2" 
+      href={'/products/new'}>Add new product</Link>
        <table className="basic mt-2">
         <thead>
           <tr>
