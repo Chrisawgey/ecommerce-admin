646db4e926019fda166ea5527d8fc04ab7b892ee
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-12 09:02:23 -0400

    adding

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,6 @@
 import multiparty from 'multiparty';
+import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+const bucketName = 'chriz-next-ecommerce'
 
 export default async function handle(req,res) {
         const form = new multiparty.Form();
@@ -9,7 +11,16 @@ export default async function handle(req,res) {
         });
     });
     console.log('length:', files.file.length);
-    console.log(fields);
+    const client = new S3Client({
+        region: 'us-east-2',
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },  
+    });
+    client.send(new PutObjectCommand({
+        Bucket: bucketName,
+    }));
     return res.json('ok');
 }
 
