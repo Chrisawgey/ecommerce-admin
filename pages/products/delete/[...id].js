321f416df02033f499d70c693f863d181b0be8a4
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-11 20:57:12 -0400

    id

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -18,13 +18,19 @@ export default function DeleteProductPage() {
     function goBack() {
         router.push('/products');
     }
+    async function deleteProduct() {
+        await axios.delete('/api/products?id'+id);
+        goBack();
+    }
     return (
         <Layout>
             <h1 className="text-center">Do you really want to delete 
             &nbsp;"{productInfo?.title}"?
             </h1>
             <div className="flex gap-2 justify-center">
-            <button className="btn-red">Yes</button>
+            <button 
+            onClick={deleteProduct()}
+            className="btn-red">Yes</button>
             <button 
             className="btn-default"
             onClick={goBack}>
