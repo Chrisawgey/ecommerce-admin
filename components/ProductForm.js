00085d90dc5d0b9e6cc5119995e16ee36bea18d8
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-11 21:43:39 -0400

    adding img

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm ({
     title:existingTitle,
     description:existingDescription,
     price:existingPrice,
+    images,
     }) {
     const [title,setTitle] = useState(existingTitle || '');
     const [description,setDescription] = useState(existingDescription || '');
@@ -17,14 +18,23 @@ export default function ProductForm ({
         ev.preventDefault();
         const data = {title,description,price};
         if (_id) {
+            //update
             await axios.put('/api/products', {...data,_id});
         } else {
+            //create
             await axios.post('/api/products', data)
         }
         setGoToProducts(true);
     }
     if (goToProducts) {
         router.push('/products');
+    }
+    ///this is the function to uplaod a pic
+    function uplaodImages(ev) {
+        const files = ev.target?.files;
+        if (files?.length > 0) {
+
+        }
     }
     return (
             <form onSubmit={saveProduct}>
@@ -34,8 +44,25 @@ export default function ProductForm ({
             type="text" 
             placeholder="product name" 
             value={title} 
-            onChange={ev => setTitle(ev.target.value)}
-            />
+            onChange={ev => setTitle(ev.target.value)}/>
+            <label>
+                Photos
+            </label>
+            <div className="mb-2">
+                <label className="w-24 h-24 cursor-pointer flex items-center justify-center text-sm gap-1
+                text-gray-500 rounded-lg bg-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75" />
+                </svg>
+                <div>
+                Upload
+                </div>
+                <input type="file" onChange={uplaodImages} className="hidden"/>
+                </label>
+                {!images?.length && (
+                    <div>No photos available for this product</div>
+                )}
+            </div>
             <label>Description</label>
             <textarea 
             placeholder="description"
