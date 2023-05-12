6699d47e5c5d071cf207f6977283cb0e0f0d0086
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-11 22:21:39 -0400

    LONG bug FIX

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -34,7 +34,9 @@ export default function ProductForm ({
         const files = ev.target?.files;
         if (files?.length > 0) {
             const data = new FormData();
-            files.forEach(file => data.append('file', file));
+            for (const file of files) {
+                data.append('file', file);
+            }
             const res = await axios.post('/api/upload', data);
             console.log(res.data);
         }
