19a5b21314fdb480f6245cd980f574cc2319d5b7
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-15 16:47:48 -0400

    creating button and form

--- pages/categories.js
+++ pages/categories.js
@@ -1,9 +1,16 @@
 import Layout from "@/components/layout";
 
 export default function Categories() {
+    function saveCategories(){}
     return(
         <Layout>
             <h1>Categories</h1>
+            <label>New category name</label>
+            <form onSubmit={saveCategories} className="flex gap-1">
+            <input className="mb-0" type="text" 
+            placeholder={'Category Name'}/>
+            <button type="submit" className="btn-primary">Save</button>
+            </form>
         </Layout>
     )
 }
