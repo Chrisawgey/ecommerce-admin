cea01b32810b07383c55ec852110a71bb50f22fd
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-16 16:09:47 -0400

    .ev

--- pages/categories.js
+++ pages/categories.js
@@ -4,7 +4,8 @@ import { useState } from "react";
 
 export default function Categories() {
     const [ name,setName ] = useState ('');
-    async function saveCategories(){
+    async function saveCategories(ev){
+        ev.preventDefault();
         await axios.post('/api/categories' , {name});
         setName('');
     }
