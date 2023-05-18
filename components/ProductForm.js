ba9d9970615fd6f3e190185623335cd9867c1e5d
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-17 22:43:22 -0400

    category for products

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useRouter} from "next/router"
 import axios from "axios";
 import Spinner from "@/components/Spinner";
@@ -10,17 +10,25 @@ export default function ProductForm ({
     description:existingDescription,
     price:existingPrice,
     images:existingImages,
+    category:assignedCategory,
     }) {
     const [title,setTitle] = useState(existingTitle || '');
     const [description,setDescription] = useState(existingDescription || '');
     const [images,setImages] = useState(existingImages || '');
+    const [category,setCategory] = useState(assignedCategory || '');
     const [price,setPrice] = useState(existingPrice || '');
     const [goToProducts,setGoToProducts] = useState(false);
     const [isUploading,SetIsUploading] = useState(false);
+    const [categories,setCategories] = useState([]);
     const router = useRouter();
+    useEffect(() => {
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data);
+        })
+    }, []);
     async function saveProduct(ev){
         ev.preventDefault();
-        const data = {title,description,price,images};
+        const data = {title,description,price,images,category};
         if (_id) {
             //update
             await axios.put('/api/products', {...data,_id});
@@ -61,6 +69,15 @@ export default function ProductForm ({
             placeholder="product name" 
             value={title} 
             onChange={ev => setTitle(ev.target.value)}/>
+            <label>Category</label>
+            <select value={category} 
+            onChange={ev => setCategory(ev.target.value)}>
+                <option value="">Uncategorized</option>
+                {categories.length > 0 && categories.map(c => (
+                    <option value={c._id}>{c.name}</option>
+                ))}
+
+            </select>
             <label>
                 Photos
             </label>
--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,11 @@
-const { Schema, model, models } = require("mongoose");
+import mongoose, { Schema, model, models } from "mongoose";
 
 const ProductSchema = new Schema({
     title: {type: String, required: true},
     description: String,
     price: {type: Number, required: true},
     images: [{type:String}],
+    category: {type:mongoose.Types.ObjectId, ref:'Category'},
 
 });
 
