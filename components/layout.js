6bd5cb3e34440dde4eed837aec8b1fe94a625102
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-25 16:48:29 -0400

    logo adding

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import Nav from "@/components/nav";
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useState } from "react";
+import Logo from "./Logo";
 
 export default function Layout({children}) {
   const [showNav,setShowNav] = useState(false);
@@ -16,12 +17,15 @@ export default function Layout({children}) {
   }
   return (
     <div className="bg-bgGray min-h-screen ">
+    <div className="block md:hidden">
     <button onClick={() => setShowNav(true)}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     </svg>
     </button>
-       <div className="flex">
+    <Logo />
+  </div>
+     <div className="flex">
       <Nav show={showNav}/>
        <div className="bg-white  grow mt-2 mr-2 mb-2 rounded-lg p-4">
         {children}
