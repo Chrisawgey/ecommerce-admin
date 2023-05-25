60789ae338a34ce69f826f5a972fe719362b2383
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-25 15:43:48 -0400

    making hamburger

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,9 @@
 import Nav from "@/components/nav";
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useState } from "react";
 
 export default function Layout({children}) {
+  const [showNav,setShowNav] = useState(false);
   const { data: session } = useSession()
   if (!session) {
     return (
@@ -14,9 +16,13 @@ export default function Layout({children}) {
   }
   return (
     <div className="bg-bgGray min-h-screen ">
-    <button>test</button>
+    <button onClick={() => setShowNav(true)}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>
+    </button>
        <div className="flex">
-      <Nav />
+      <Nav show={showNav}/>
        <div className="bg-white  grow mt-2 mr-2 mb-2 rounded-lg p-4">
         {children}
        </div>
