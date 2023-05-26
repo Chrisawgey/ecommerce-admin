582bb073b41e887c563b457d3bbdaf2ea849922c
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-26 15:17:09 -0400

    done with fixing center logo

--- components/layout.js
+++ components/layout.js
@@ -17,13 +17,13 @@ export default function Layout({children}) {
   }
   return (
     <div className="bg-bgGray min-h-screen">
-    <div className="block md:hidden flex items-center ">
+    <div className="block md:hidden flex items-center p-4">
     <button onClick={() => setShowNav(true)}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     </svg>
     </button>
-    <div className="flex grow">
+    <div className="flex grow justify-center mr-6">
     <Logo />
     </div>
   </div>
