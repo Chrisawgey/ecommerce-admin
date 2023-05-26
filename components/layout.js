f4c5cb4652b49be7adf30d1076f566571dbecf19
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-26 15:04:36 -0400

    starting logo flex

--- components/layout.js
+++ components/layout.js
@@ -16,8 +16,8 @@ export default function Layout({children}) {
     );
   }
   return (
-    <div className="bg-bgGray min-h-screen ">
-    <div className="block md:hidden">
+    <div className="bg-bgGray min-h-screen">
+    <div className="block md:hidden flex">
     <button onClick={() => setShowNav(true)}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
