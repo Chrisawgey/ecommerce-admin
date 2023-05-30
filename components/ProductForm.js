7f0d883206bb7abf2e595764a7723c3a8e4510f9
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-05-30 15:52:38 -0400

    img tag

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -137,13 +137,13 @@ export default function ProductForm({
                 <Spinner />
                 </div>
             )}
-                <label className=" w-24 h-24 cursor-pointer flex items-center justify-center text-sm gap-1
-                text-gray-500 rounded-sm bg-white shadow-sm border border-gray-200">
+                <label className=" w-24 h-24 cursor-pointer flex flex-col items-center justify-center text-sm gap-1
+                text-primary rounded-sm bg-white shadow-sm border border-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75" />
                 </svg>
                 <div>
-                Upload
+                Add Image
                 </div>
                 <input type="file" onChange={uplaodImages} className="hidden"/>
                 </label>
