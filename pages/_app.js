3c0e175f402320ab6ca744241b4887fa2e62106f
Chrisawgey <124189207+Chrisawgey@users.noreply.github.com>
2024-01-09 17:34:17 -0500

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
 export default function App({
   Component, pageProps: { session, ...pageProps }
