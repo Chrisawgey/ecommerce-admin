650765c428352fbb1762115ff71ba4120aa48863
Chrisawgey <chrisvpopoca@gmail.com>
2024-01-30 22:39:50 -0500

    featured is working good so far

--- pages/settings.js
+++ pages/settings.js
@@ -1,79 +1,65 @@
-import Layout from "@/components/layout";
-import {useEffect, useState} from "react";
-import axios from "axios";
 import Spinner from "@/components/Spinner";
-import {withSwal} from "react-sweetalert2";
+import Layout from "@/components/layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { withSwal } from "react-sweetalert2";
 
 function SettingsPage({swal}) {
   const [products, setProducts] = useState([]);
   const [featuredProductId, setFeaturedProductId] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [shippingFee, setShippingFee] = useState('');
+  const [productsLoading, setProductsLoading] = useState(false);
+  const [featuredLoading, setFeaturedLoading] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true);
-    fetchAll().then(() => {
-      setIsLoading(false);
+    setProductsLoading(true);
+    axios.get('/api/products').then(res => {
+      setProducts(res.data);
+      setProductsLoading(false);
+
     });
+    setFeaturedLoading(true);
+    axios.get('/api/settings?name=featuredProductId').then(res => {
+      setFeaturedProductId(res.data.value);
+      setFeaturedLoading(false);
+    })
   }, []);
 
-  async function fetchAll() {
-    await axios.get('/api/products').then(res => {
-      setProducts(res.data);
-    });
-    await axios.get('/api/settings?name=featuredProductId').then(res => {
-      setFeaturedProductId(res.data.value);
-    });
-    await axios.get('/api/settings?name=shippingFee').then(res => {
-      setShippingFee(res.data.value);
-    });
-  }
-
-  async function saveSettings() {
-    setIsLoading(true);
+  async function saveSettings(){
     await axios.put('/api/settings', {
       name: 'featuredProductId',
       value: featuredProductId,
-    });
-    await axios.put('/api/settings', {
-      name: 'shippingFee',
-      value: shippingFee,
-    });
-    setIsLoading(false);
-    await swal.fire({
-      title: 'Settings saved!',
-      icon: 'success',
+    }).then(() => {
+      swal.fire({
+        title: 'Settings Saved!',
+        icon: 'success',
+      })
     });
   }
 
   return (
     <Layout>
       <h1>Settings</h1>
-      {isLoading && (
+      {(productsLoading || featuredLoading) && (
         <Spinner />
       )}
-      {!isLoading && (
+      {(!productsLoading && !featuredLoading) && (
         <>
-          <label>Featured product</label>
-          <select value={featuredProductId} onChange={ev => setFeaturedProductId(ev.target.value)}>
-            {products.length > 0 && products.map(product => (
-              <option value={product._id}>{product.title}</option>
-            ))}
-          </select>
-          <label>Shipping price (in usd)</label>
-          <input type="number"
-                 value={shippingFee}
-                 onChange={ev => setShippingFee(ev.target.value)}
-          />
-          <div>
-            <button onClick={saveSettings} className="btn-primary">Save settings</button>
-          </div>
+        <label>Featured product</label>
+      <select value={featuredProductId} onChange={ev => setFeaturedProductId(ev.target.value)}>
+        {products.length > 0 && products.map(product => (
+          <option value={product._id}>{product.title}</option>
+        ))}
+      </select>
+      <div>
+        <button onClick={saveSettings} className="btn-primary">Save Settings</button>
+      </div>
         </>
       )}
+      
     </Layout>
-  );
+  )
 }
 
 export default withSwal(({swal}) => (
-  <SettingsPage swal={swal} />
+  <SettingsPage swal={swal}/>
 ));
